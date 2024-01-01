b52cf2fa09f58b686925875c2d705e89d0a205da
Emmanuel Lorenzotti <Manu@AbletonUman.local>
2024-01-01 03:54:21 +0100

    First commit

---
+++ pkg/render/render.go
@@ -0,0 +1,78 @@
+package render
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/elorenzotti/bookings/pkg/config"
+	"github.com/elorenzotti/bookings/pkg/models"
+)
+
+var app *config.AppConfig
+
+//NewTemplates sets the config for the 
+func NewTemplates(a *config.AppConfig){
+	app = a
+}
+
+// RenderTemplate renders template using html
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache{
+
+	// get template cache
+	tc = app.TemplateCache
+	} else{
+		tc, _= CreateTemplateCache()
+	}
+
+	// get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("could not get template frome template cache")
+	}
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, td)
+	
+	// render the template
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	// get all of the file named *.page.tmpl in the folder template
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
+}
