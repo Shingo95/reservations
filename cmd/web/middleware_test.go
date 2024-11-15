08daf22bf0b7e217aefe6530e58b5a79a4090f9b
Emmanuel <manu@MacBook-Pro-de-Emmanuel.local>
2024-11-15 23:25:14 +0100

    feature: added SessionLoad Testing

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -17,3 +17,16 @@ func TestNoSurf(t *testing.T) {
 		t.Errorf("Type is not http Handler but is %T", v)
 	}
 }
+
+func TestSessionLoad(t *testing.T) {
+
+	var myH myHandler
+
+	h := SessionLoad(&myH)
+	switch v := h.(type) {
+	case http.Handler:
+		// Do Nothing
+	default:
+		t.Errorf("Type is not http Handler but is %T", v)
+	}
+}
